e362fe621d1135d30cadeef7873978978cefd29e
Marcelo Romitti <romittimarcelo@gmail.com>
2021-04-29 17:15:20 -0300

    First commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddCategory = ( { setCategories }) => {
+
+const [inputValue, setInputValue] = useState('');
+
+    const handleInputValue = ( e ) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSubmit = ( e ) => {
+        e.preventDefault();
+     
+        if( inputValue.trim().length > 2 ) {
+            setCategories( cats => [ inputValue, ...cats]);
+            setInputValue('');
+        }
+      
+    }
+
+    return (  
+        <form onSubmit = { handleSubmit }>
+            <input
+                type = "text"
+                onChange = { handleInputValue }
+                value = { inputValue }
+            ></input>
+
+        </form>
+
+
+    );
+}
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
+ 
+export default AddCategory;
---
+++ src/components/GifGrid.js
@@ -0,0 +1,29 @@
+import React from 'react'
+// import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
+import { useFetchGifs } from '../hooks/useFetchGifs'
+
+export const GifGrid = ( { category }) => {
+
+    const { data:images, loading } = useFetchGifs( category );
+
+    return (
+        <>
+    
+            <h3>{ category }</h3>
+
+            <div className="card-grid">
+                
+                { loading && <p>Loading..</p> }
+
+                    {
+                        images.map( img  => 
+                            <GifGridItem
+                            key = { img.id }
+                            {... img } />
+                    )}
+
+            </div>
+        </>
+    )
+}
